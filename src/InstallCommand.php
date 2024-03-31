ba346f51b56a2269335f981dc7665f5d9464a840
Musfiqur rahaman <m.r.istiak2007@gmail.com>
2024-03-31 22:17:26 +0600

    Fixed Daahborad Route

--- src/InstallCommand.php
+++ src/InstallCommand.php
@@ -20,6 +20,11 @@ class InstallCommand extends Command
         $fs = new Filesystem();
         $fs->copyDirectory(__DIR__.'/../Admin', resource_path('js/Pages/Admin'));
         $fs->copy(__DIR__.'/administration.php', config_path('administration.php'));
+
+        $routes = $fs->get(base_path('routes/web.php'));
+        $pattern = '/Route::get\(\'\/dashboard\',\s*function\s*\(\)\s*{[^}]+}\)->middleware\(\[\s*\'auth\',\s*\'verified\'\]\)->name\(\'dashboard\'\);/';
+        file_put_contents(base_path('routes/web.php'), preg_replace($pattern, '', $routes));
+        
         Artisan::call('vendor:publish', ['--provider' => 'Administration\ServiceProvider']);
         Artisan::call('migrate:fresh', ['--seed' => true, '--seeder' => 'Administration\RoleSeeder']);
     }
